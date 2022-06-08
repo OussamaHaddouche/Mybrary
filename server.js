268768a29b8b72eafbb5b67c2ef4dc9f379fa6a5
Oussama Haddouche <oussama1haddouche2@gmail.com>
2022-06-08 20:54:53 +0100

    intial author routes

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-if(process.env.NODE_ENV !== "production") {
-    require("dotenv").config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
+  useNewUrlParser: true,
 });
-const db = mongoose.connection
-db.on("error", err => console.error(error))
-db.once("open", () => console.log("connected to mongoose"))
+const db = mongoose.connection;
+db.on("error", (err) => console.error(error));
+db.once("open", () => console.log("connected to mongoose"));
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
-
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
